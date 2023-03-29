12cc3f752bb5bb0a819e7566ff9d202ccb8bcf45
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-29 15:53:03 -0300

    Resolução dos exercícios

---
+++ script.js
@@ -0,0 +1,30 @@
+// //Pratica 1
+// let numero = Number(prompt("Digite um número"))
+// let resposta = 0
+// while (numero !== 0) {
+//     console.log(numero)
+//     resposta = resposta + numero
+//     numero = Number(prompt("Digite um número"))
+// }
+
+// console.log(`A soma dos números digitados é: ${resposta}`)
+
+//Pratica 2
+
+// let numeroUser = Number(prompt("Digite um número para contagem"))
+
+// for(i=0;i<=numeroUser;i++){
+//     console.log(i)
+// }
+
+//Prática 3
+const numeros = [38, 23, 2, 4, 8, 9, 32]
+
+// for(let i=0; i<numeros.length;i++){
+//     console.log(`O elemento de índice ${i} é ${numeros[i]}`)
+// }
+let i=0
+while(i<numeros.length){
+    console.log(`O elemento de índice ${i} é ${numeros[i]}`)
+    i++
+}
